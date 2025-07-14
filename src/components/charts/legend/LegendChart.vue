b81824af89f2953c341c92c5a442310bea3b1f19
Gabriel Soares <gsoarescassimiro@gmail.com>
2025-07-14 00:37:35 -0300

    🚀feat(legend chart): separated legend to solo component

---
+++ src/components/charts/legend/LegendChart.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ul :class="['charts-css', 'legend', 'legend-square']"
+            :style="{ fontSize: '1rem', lineHeight: '1.5'}">
+            <li v-for="(item, index) in data" :key="index">
+                {{ item?.dataPresentation }}%
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    data: {
+        type: Array,
+        required: true
+    }
+});
+</script>
+
+
+<style></style>
